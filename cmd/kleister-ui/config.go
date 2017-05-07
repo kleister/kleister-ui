37dc34cb0a19b03027bf5160d79901a1acec4c3c
Thomas Boerger <thomas@webhippie.de>
2017-05-07 21:59:20 +0200

    Switched to urfave/cli v2

--- cmd/kleister-ui/config.go
+++ cmd/kleister-ui/config.go
@@ -1,15 +1,17 @@
 package main
 
 type server struct {
-	Host        string
-	Addr        string
-	Cert        string
-	Key         string
-	Endpoint    string
-	Static      string
-	Storage     string
-	LetsEncrypt bool
-	Pprof       bool
+	Host          string
+	Addr          string
+	Cert          string
+	Key           string
+	Endpoint      string
+	Static        string
+	Storage       string
+	LetsEncrypt   bool
+	StrictCurves  bool
+	StrictCiphers bool
+	Pprof         bool
 }
 
 type config struct {
