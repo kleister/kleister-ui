09d50b3d53a5ce4972afebccf1ad8c2810144169
Thomas Boerger <thomas@webhippie.de>
2024-02-11 23:51:23 +0100

    feat: drop unused init code

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,5 @@
 </template>
 
 <script setup>
-import { onMounted } from "vue";
-import { initFlowbite } from "flowbite";
-
-import GeneralLayout from "./components/GeneralLayout.vue";
-
-onMounted(() => {
-  initFlowbite();
-});
+import { GeneralLayout } from "./components";
 </script>
-
-<style scoped></style>
