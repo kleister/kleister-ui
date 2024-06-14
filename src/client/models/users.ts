9435557daab5b9959ab7bb25d54b62b590880697
Thomas Boerger <thomas@webhippie.de>
2024-06-14 08:50:37 +0200

    feat: drop old generated client

--- src/client/models/users.ts
+++
@@ -1,8 +0,0 @@
-/* generated using openapi-typescript-codegen -- do no edit */
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-import type { user } from "./user";
-export type users = {
-  users?: Array<user>;
-};
